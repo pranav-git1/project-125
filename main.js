b6383cd12fb11b53702513a9afc14f96df52e64a
pranav-git1 <77086184+pranav-git1@users.noreply.github.com>
2021-07-01 08:43:02 +0530

    uploaded project

---
+++ main.js
@@ -0,0 +1,39 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(500, 500);
+    canvas.position(600, 150);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet is initialized');
+}
+
+function draw() {
+    background('#808080');
+    fill('#FF0000');
+    stroke('#FF0000');
+    text('PRANAV', noseX, noseX);
+    textSize(difference);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX);
+        console.log("noseY = " + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+    }
+}
